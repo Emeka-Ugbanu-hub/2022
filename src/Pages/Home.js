4df19497614aad3a450c26f9ce8ff52cfa8a28eb
Emeka Ugbanu <65762678+Emeka-Ugbanu-hub@users.noreply.github.com>
2024-12-31 10:27:05 -0800

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,8 +20,7 @@ function Home() {
             gl.setClearColor(new THREE.Color('#020207'));     
           }}
         >   
-          <fog attach="fog" args={['#FFD7B5', 20, 190]} />       
-          <pointLight distance={100} intensity={0.5} color="#FFD7B5" />       
+          
           <Year />       
           <Particles count={isMobile ? 5000 : 10000} />       
           <Sparks count={30} colors={['#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff', 'lightpink', 'lightblue']} />       
