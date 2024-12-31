c0ff6709db3238ef2e9c223c0980e86c19f5deb9
Emeka Ugbanu <65762678+Emeka-Ugbanu-hub@users.noreply.github.com>
2024-12-31 09:55:33 -0800

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
     }}
    >  
 <fog attach="fog" args={['white', 50, 190]} />
-      <pointLight distance={100} intensity={4} color="white" />
+      <pointLight distance={100} intensity={2} color="white" />
       <Year/>
       <Particles count={isMobile ? 5000 : 10000}/>
       <Sparks count={30}  colors={['#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff', 'lightpink', 'lightblue']} />
